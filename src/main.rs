7dae9e0537f90d9a01ea1a91433908252f3b9b0f
Timo Schulz <timo.schulz@hhu.de>
2023-12-13 13:55:11 +0100

    Mapped mulliken charges to fragments and computed effective dipole lengths and angle between vectors.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 use std::env;
 use std::error::Error;
 use std::f32::EPSILON;
+use std::f32::consts::PI;
 use std::fs;
 use std::io::Read;
 fn print_usage() {
@@ -28,7 +29,7 @@ impl Fragments {
 }
 
 /// Represents a dipole vector, i.e. a charge weighted cartesian vector.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Dipole{compnents: [f32;3]}
 
 impl Default for Dipole {
@@ -41,7 +42,9 @@ fn parse_fragment_toml_file(frag_file: &mut str) -> Result<Fragments, Box<dyn Er
     let mut fh = fs::File::open(frag_file)?;
     let mut buf = String::new();
     fh.read_to_string(&mut buf)?;
-    let frags: Fragments = toml::from_str(&buf)?;
+    let mut frags: Fragments = toml::from_str(&buf)?;
+    frags.fragment1.sort();
+    frags.fragment2.sort();
     Ok(frags)
 }
 
@@ -84,6 +87,18 @@ fn parse_dipole_from_prp(
     Ok(dip_vec)
 }
 
+fn map_dips_to_fragment(frag: Vec<usize>, nmbrs_nd_dps: &[Dipole]) -> Vec<Dipole> {
+    let mut dipls_per_frag: Vec<Dipole> = Vec::with_capacity(frag.capacity());
+    for indx in frag {
+        dipls_per_frag.push(nmbrs_nd_dps[indx-1])
+    }
+    dipls_per_frag
+}
+
+fn dot_prd(vec1: (f32, f32, f32), vec2: (f32,f32,f32)) -> f32 {
+    vec1.0 * vec2.0 + vec1.1 * vec2.1 + vec1.2 * vec2.2
+}
+
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     if args.len() < 4 {
@@ -93,8 +108,23 @@ fn main() {
     let prp_file = args.pop().unwrap();
     let mut frag_file = args.pop().unwrap();
     let frags = parse_fragment_toml_file(&mut frag_file).unwrap();
-    let nmbrs_nd_chrgs: Vec<Dipole> =
+    let nmbrs_nd_dps: Vec<Dipole> =
         parse_dipole_from_prp(&prp_file, state_label).unwrap();
+    let dipoles_fragmnt1: Vec<Dipole> = map_dips_to_fragment(frags.fragment1, &nmbrs_nd_dps);
+    let dipoles_fragmnt2: Vec<Dipole> = map_dips_to_fragment(frags.fragment2, &nmbrs_nd_dps);
+    let zero = (0_f32, 0_f32, 0_f32);
+    let total1 = dipoles_fragmnt1.iter().fold(zero, |acc, dpl| { (acc.0 + dpl.compnents[0], acc.1 + dpl.compnents[1], acc.2 + dpl.compnents[2]) });
+    let total2 = dipoles_fragmnt2.iter().fold(zero, |acc, dpl| { (acc.0 + dpl.compnents[0], acc.1 + dpl.compnents[1], acc.2 + dpl.compnents[2]) });
+    let total = (total1.0 + total2.0, total1.1 + total2.1, total1.2 + total2.2);
+    println!("Fragment 1 has partial t-dipole: {:?}", total1);
+    println!("Fragment 2 has partial t-dipole: {:?}", total2);
+    println!("Total t-dipole is              : {:?}", total);
+    let len_total1:f32 = dot_prd(total1, total1).sqrt();
+    let len_total2:f32 = dot_prd(total2, total2).sqrt();
+    let dot_12: f32= dot_prd(total1, total2);
+    println!("Length of partial t-dipole on 1: {:?}", len_total1);
+    println!("Length of partial t-dipole on 2: {:?}", len_total2);
+    println!("Angle between partial dipoles  : {:?}", (dot_12/(len_total1 * len_total2)).acos() * 180.0_f32/PI);
 }
 
 #[cfg(test)]
